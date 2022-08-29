982a79ab5f01bb0245e6c25265c79f4791c9d355
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-08-29 23:55:55 +0530

    js changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function ButtonAddTask(){
     var add_task = document.getElementsById("add-task").value;
-    document.getElementById("task1").innerHTML = "hello" + add_task;
+    document.getElementById("task1").innerHTML =add_task;
 }
