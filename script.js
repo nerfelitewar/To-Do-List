a81c374ed0d7f001d7012ac0a378b23df34101b1
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-09-06 03:17:18 +0530

    modified html and added function, js onclick function and checked thing, css negligible

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
 function ButtonAddTask(){
-    var add_task = document.getElementsById("add-task").value;
-    document.getElementById("task1").innerHTML =add_task;
+    var a= document.getElementById("add-task").value;
+    document.getElementById("task1").innerHTML=a;
+    
+
+}
+
+
+
+function Donetask1(){
+    if(document.getElementById('list1').checked=true){
+        document.getElementById('task1').innerHTML='<s>'+document.getElementById("add-task").value+'<s>';
+    }else if(document.getElementById('list1').checked=false){
+        document.getElementById('task1').innerHTML=document.getElementById("add-task").value;
+    }
+
+}
+function Donetask2(){
+    if(document.getElementById('list2').checked=true){
+        document.getElementById('task2').innerHTML='<s>'+document.getElementById("add-task").value+'<s>';
+    }else if(document.getElementById('list2').checked=false){
+        document.getElementById('task2').innerHTML=document.getElementById("add-task").value;
+    }
+
+}
+function Donetask3(){
+    if(document.getElementById('list3').checked=true){
+        document.getElementById('task3').innerHTML='<s>'+document.getElementById("add-task").value+'<s>';
+    }else if(document.getElementById('list3').checked=false){
+        document.getElementById('task3').innerHTML=document.getElementById("add-task").value;
+    }
+
+}
+function Donetask4(){
+    if(document.getElementById('list4').checked=true){
+        document.getElementById('task4').innerHTML='<s>'+document.getElementById("add-task").value+'<s>';
+    }else if(document.getElementById('list4').checked=false){
+        document.getElementById('task4').innerHTML=document.getElementById("add-task").value;
+    }
+
+}
+function Donetask5(){
+    if(document.getElementById('list5').checked=true){
+        document.getElementById('task5').innerHTML='<s>'+document.getElementById("add-task").value+'<s>';
+    }else if(document.getElementById('list5').checked=false){
+        document.getElementById('task5').innerHTML=document.getElementById("add-task").value;
+    }
+
 }
